9280346993df8ccf87615ebe9c98919570abcdef
Venkat Nagappan <venkat.nagappan@checkit.net>
2020-03-10 14:40:06 +0000

    TransactionsList API added

--- restful_resources/routes.py
+++ restful_resources/routes.py
@@ -1,7 +1,10 @@
-from restful_resources.account import AccountsApi
+from restful_resources.account import AccountsApi, DistinctAccountTypesApi
 from restful_resources.categories import CategoriesApi
+from restful_resources.transactions import TransactionsListApi
 
 
 def initialize_routes(api):
     api.add_resource(AccountsApi, '/api/accounts')
     api.add_resource(CategoriesApi, '/api/categories')
+    api.add_resource(DistinctAccountTypesApi, '/api/distinctaccounts')
+    api.add_resource(TransactionsListApi, '/api/listtransactions')
